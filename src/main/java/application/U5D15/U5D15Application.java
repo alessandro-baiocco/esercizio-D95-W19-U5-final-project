acb3803daf45af781d6a0637b446052192af2bd5
alessandro baiocco <baioccoalessandro97@gmail.com>
2023-11-17 10:06:13 +0100

    Initial commit

---
+++ src/main/java/application/U5D15/U5D15Application.java
@@ -0,0 +1,13 @@
+package application.U5D15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5D15Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5D15Application.class, args);
+	}
+
+}
---
+++ src/test/java/application/U5D15/U5D15ApplicationTests.java
@@ -0,0 +1,13 @@
+package application.U5D15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5D15ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
