c0db280b569561dfebc4bf69e2bee78f7adfa77b
alessandro baiocco <baioccoalessandro97@gmail.com>
2023-11-17 10:26:49 +0100

    configurazione al db e a cloudinary aggiunta

--- src/main/java/application/U5D15/entities/Event.java
+++ src/main/java/application/U5D15/entities/Event.java
@@ -1,14 +1,24 @@
 package application.U5D15.entities;
 
+import jakarta.persistence.*;
+
 import java.util.Date;
 import java.util.List;
 
+@Entity
 public class Event {
+    @Id
+    @GeneratedValue
+    private int id;
     private String title;
     private String description;
     private Date data;
     private String place;
     private int numberMax;
+    @ManyToMany
+    @JoinTable(name = "event_user",
+            joinColumns = @JoinColumn(name = "event_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id"))
     private List<User> participants;
 
 
--- src/main/java/application/U5D15/entities/User.java
+++ src/main/java/application/U5D15/entities/User.java
@@ -1,13 +1,25 @@
 package application.U5D15.entities;
 
+import application.U5D15.enums.Ruolo;
+import jakarta.persistence.*;
+
 import java.util.List;
 
+@Entity
 public class User {
+    @Id
+    @GeneratedValue
+    private int id;
     private String name;
     private String lastName;
     private String email;
     private String userName;
+    @Enumerated(EnumType.STRING)
     private Ruolo role;
+    @ManyToMany
+    @JoinTable(name = "event_user",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "event_id"))
     private List<Event> events;
 
 }
